dc21e70beadfa2b75e380a5a382803b7b12e57f0
Elton P. Alves <Elton P. Alves@ELTONPA>
2017-03-09 20:11:21 -0300

    vooltando ao trabalho master

--- projetogit/src/projetogit/git.java
+++ projetogit/src/projetogit/git.java
@@ -3,6 +3,8 @@ package projetogit;
 public class git {
 	public static void main(String[]args){
 		System.out.print("ola mundo teste 2.... alteracao3");
+		System.out.print("ola mundo teste 2.... alteracao master");
+		
 		
 	}
 
